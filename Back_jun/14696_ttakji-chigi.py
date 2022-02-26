cfe4d98b34e077007e7ca7da082d10543ce635b6
jayho-k <sox3579@gmail.com>
2022-02-27 05:26:45 +0900

    0227

--- Back_jun/14696_ttakji-chigi.py
+++ Back_jun/14696_ttakji-chigi.py
@@ -3,37 +3,39 @@
 
 '''
 
-r = int(input())
+# r = int(input())
 
-for _ in range(r):
-    al = list(map(int, input().split()))
-    bl = list(map(int, input().split()))
-    mn_cnt = min(al[0], bl[0])
+# for _ in range(r):
+#     al = list(map(int, input().split()))
+#     bl = list(map(int, input().split()))
+#     mn_cnt = min(al[0], bl[0])
 
-    alst = sorted(al[1:], reverse=True)
-    blst = sorted(bl[1:], reverse=True)
+#     alst = sorted(al[1:], reverse=True)
+#     blst = sorted(bl[1:], reverse=True)
 
-    for i in range(mn_cnt):
-        if alst[i] > blst[i]:
-            print('A')
-            break
+#     for i in range(mn_cnt):
+#         if alst[i] > blst[i]:
+#             print('A')
+#             break
 
-        elif alst[i] < blst[i]:
-            print('B')
-            break
+#         elif alst[i] < blst[i]:
+#             print('B')
+#             break
 
-    else:
-        if al[0] > bl[0]:
-            print('A')
+#     else:
+#         if al[0] > bl[0]:
+#             print('A')
 
-        elif al[0] < bl[0]:
-            print('B')
-        else:
-            print('D')
-
-
+#         elif al[0] < bl[0]:
+#             print('B')
+#         else:
+#             print('D')
 
 
+n = 5
+lst = [x for x in range(1,n+1)]
+print(lst)
+print(list(map(list,enumerate(lst))))
 
 
 
--- Back_jun/swea_cheeze.py
+++ Back_jun/swea_cheeze.py
@@ -12,24 +12,31 @@ from collections import deque
 T = int(input())
 for tc in range(1, T+1):
     n, m = map(int, input().split())
-    lst = list(enumerate(map(int, input().split()), start = 1))
+    l = map(int, input().split())
+    lst = list(map(list, enumerate(l, start=1)))
+    # print(lst)
+
     rst = deque(lst[n:])
     q = deque(lst[:n])
     
     for n_rst in rst:
         while 1:
-            q.rotate(-1)
-            q[-1][1]=q[-1][1]//2
-            print(q)
-            if q[-1][1] == 0:
-                q.pop()
-                q.append(n_rst)
-                print(q)
+            q[0][1]=q[0][1]//2
+            # print(q)
+            if q[0][1] == 0:
+                q.popleft()
+                q.appendleft(n_rst)
+                # print('end',q)
                 break
+            else:
+                q.rotate(-1)
     
     while q:
-        q.rotate(-1)
-        q[-1][1]=q[-1][1]//2
-        print(q)
-        if q[-1][1] == 0:
-            ans = q.pop()
+        q[0][1]=q[0][1]//2
+        # print(q)
+        if q[0][1] == 0:
+            ans = q.popleft()
+        else:
+            q.rotate(-1)
+    
+    print(f'#{tc} {ans[0]}')
