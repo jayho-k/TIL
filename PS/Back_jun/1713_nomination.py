f8b557c99977f47a312e688e5912ea1b89739f53
jayho <sox3579@gmail.com>
2022-05-08 22:21:46 +0900

    0508

---
+++ PS/Back_jun/1713_nomination.py
@@ -0,0 +1,72 @@
+'''
+3
+9
+2 1 4 3 5 6 2 7 2
+
+3
+12
+1 2 3 2 3 1 4 1 1 2 3 4
+
+'''
+
+case = int(input())
+n = int(input())
+lst = list(map(int,input().split()))
+
+# 추천, 시간
+stu = [[0,0] for _ in range(101)]
+
+can_num = []
+for s in lst:
+
+    # 후보개수 올리기
+    if s not in can_num:
+        can_num.append(s)
+
+    # 사진틀 부족
+    # if (101-stu.count([0,0])) == case+1:
+    if len(can_num)  == case+1:
+
+        mn = 1e9
+        for m in range(101):
+            if mn>stu[m][0] and stu[m][0] != 0:
+                mn = stu[m][0]
+
+        rmv_lst =[]
+        for j in range(101):
+            if stu[j][0] == mn:
+                rmv_lst.append(j)
+
+        old = 0
+        old_i = 0
+        if len(rmv_lst) >= 2:
+            for r in rmv_lst:
+                if old < stu[r][1]:
+                    old = stu[r][1]
+                    old_i = r
+        else:
+            # print(rmv_lst)
+            old_i = rmv_lst[0]
+
+        # print(stu)
+        stu[old_i] = [0,0]
+
+        can_num.remove(old_i)
+
+
+    # 후보 올리기
+    stu[s][0] += 1
+
+    # 시간 재기
+    for i in range(101):
+        if stu[i][0] != 0:
+            stu[i][1] += 1
+
+ans = []
+for a in range(101):
+    if stu[a] != [0,0]:
+        ans.append(a)
+
+# print(stu)
+print(*ans)
+
