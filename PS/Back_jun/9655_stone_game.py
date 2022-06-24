726e0887e732b4398add54687935c157c510309c
jayho <sox3579@gmail.com>
2022-06-24 11:39:36 +0900

    0624

---
+++ PS/Back_jun/9655_stone_game.py
@@ -0,0 +1,20 @@
+'''
+3 3  ==> 6
+1 3  ==> 4
+3 1  ==> 4
+1 1  ==> 2
+
+특징1: 짝수다
+
+'''
+
+
+n = int(input())
+
+namuzi = n % 2
+
+if namuzi == 1:
+    print('SK')
+
+elif namuzi == 0:
+    print('CY')
