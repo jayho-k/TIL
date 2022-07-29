12868c6c5b6ad915f8a3a1cd130b76f92a50055f
jayho-k <sox3579@gmail.com>
2022-07-30 00:05:42 +0900

    0730

---
+++ PS/Back_jun/9019_.py
@@ -0,0 +1,26 @@
+'''
+3
+1234 3412
+1000 1
+1 16
+
+
+'''
+def D():
+    pass
+
+def S():
+    pass
+
+def L():
+    pass
+
+def R():
+    pass
+
+
+
+T = int(input())
+
+for tc in range(1,T+1):
+    pass
