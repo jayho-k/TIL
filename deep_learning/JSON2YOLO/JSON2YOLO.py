7d17278a7a9d7a155035b7c9501e4a029d57c88f
jayho-k <sox3579@gmail.com>
2022-09-15 03:59:41 +0900

    0915

--- deep_learning/JSON2YOLO/JSON2YOLO.py
+++ deep_learning/JSON2YOLO/JSON2YOLO.py
@@ -30,12 +30,11 @@ class JSON2YOLO:
         json_files = os.listdir(json_dir)
         for json_file in json_files:
             labels.append(json_file)
-        print(labels)
         return labels
 
     def _load_image_info(self,image,annotations):
         images_info = {}
-        id = image['path']
+        id = image['filename'].split('.')[0]
         for annotation in annotations:
             annotation['image_id'] = id
         filename = image['filename']
@@ -47,7 +46,6 @@ class JSON2YOLO:
 
 
     def _bbox_2_yolo(self, bbox, img_w, img_h):
-        # ms-coco는 좌상단 x, y좌표, width, height
         x, y, w, h = bbox[0], bbox[1], bbox[2], bbox[3]
         centerx = x + w / 2
         centery = y + h / 2
@@ -62,17 +60,19 @@ class JSON2YOLO:
     def _convert_anno(self,image_info,annotations):
         anno_dict = dict()
         for anno in annotations:
+            if anno.get('box')==None:
+                continue
             bbox = anno['box']
             image_id = anno['image_id']
-            class_id = anno['class']
-            middle_class = anno['middle classification']
+            class_id = anno['middle classification']
+            sub_class_id = anno['class']
             data_id = anno['data ID']
             image_name = image_info[image_id][0]
             img_w = image_info[image_id][1]
             img_h = image_info[image_id][2]
             yolo_box = self._bbox_2_yolo(bbox, img_w, img_h)
 
-            anno_info = (image_name, class_id, yolo_box, middle_class,data_id)
+            anno_info = (image_name, class_id, yolo_box, sub_class_id, data_id)
             anno_infos = anno_dict.get(image_id)
             if not anno_infos:
                 anno_dict[image_id] = [anno_info]
@@ -87,7 +87,7 @@ class JSON2YOLO:
 
         with open(tgt_anno_filename, 'w', encoding='utf-8') as f:
             for obj in anno_dict[image_id]:
-                class_id = obj[1]
+                class_id = int(list(obj[1])[1]) - 1
                 box = ['{:.6f}'.format(x) for x in obj[2]]
                 box = ' '.join(box)
                 line = str(class_id) + ' ' + box
@@ -95,16 +95,23 @@ class JSON2YOLO:
             
     def json2yolo(self):
         i = 1
+        j = 0
         total = len(self.labels)
         for label in self.labels:
-            loaded_json = json.load(open(os.path.join(os.getcwd(),'json',label), 'r', encoding='utf-8'))
+
+            loaded_json = json.load(open(os.path.join(self.json_dir,label), 'r', encoding='utf-8'))
+
             image = loaded_json['image']
             annotations = loaded_json['annotations']
-            print(image)
-            print(annotations)
-
+            if 'box' not in annotations[0]:
+                j += 1
+                print('this is polygon')
+                continue
+            
             image_info = self._load_image_info(image,annotations)
             anno_dict,image_id = self._convert_anno(image_info,annotations)
             self._save_txt(anno_dict,image_id)
             print(f'complete {i}/{total}')
             i+=1
+        print(f'complete {i}/{total}')
+        print(f'poly {j}/{total}')
--- deep_learning/YOLOv7.md
+++ deep_learning/YOLOv7.md
@@ -1,3 +1,61 @@
 # YOLOv7
 
+![](YOLOv7.assets/2022-09-11-14-25-30-image.png)
+
+- 파이토치로 구현
+
+
+
+
+
+json(coco dataset) => yaml 변환
+
+coco dataset은 하나의 json파일로 되어 있음
+
+
+
+
+
+
+
+bbox 정보가 정규화 되어 있음
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
