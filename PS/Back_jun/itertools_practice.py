2817c054c6b14a14aa8e701976e52193c8f69b99
jayho-k <sox3579@gmail.com>
2023-02-16 01:44:22 +0900

    0216

--- PS/Back_jun/itertools_practice.py
+++ PS/Back_jun/itertools_practice.py
@@ -2,21 +2,34 @@ from itertools import permutations
 from itertools import combinations
 from itertools import product
 from itertools import accumulate
+from itertools import combinations_with_replacement as cwr
+from collections import Counter
 from pprint import pprint
 
-# accumulate
-def func(x,y):
-    print(x,y)
-    return x+y
-n = 5
-# grid = [[0]*(n+1) for _ in range(n+1)]
-grid = [list(range(1,n+1)) for _ in range(n)]
-lst = list(range(1,n+1))
-print(lst)
-res_grid = []
-for _ in range(n):
-    res = list(accumulate(lst,func))
-    res_grid+=res
+
+tu = (1,2,3,4,5,1,1,1,1,1,1,1,1)
+print(Counter(tu))
+
+
+
+# print(list(cwr(range(11),10)))
+
+
+
+
+# # accumulate
+# def func(x,y):
+#     print(x,y)
+#     return x+y
+# n = 5
+# # grid = [[0]*(n+1) for _ in range(n+1)]
+# grid = [list(range(1,n+1)) for _ in range(n)]
+# lst = list(range(1,n+1))
+# print(lst)
+# res_grid = []
+# for _ in range(n):
+#     res = list(accumulate(lst,func))
+#     res_grid+=res
     
-pprint(grid)
-print(res)
+# pprint(grid)
+# print(res)
