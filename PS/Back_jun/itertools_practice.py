4ef1db73ba32fd94dc5f1fb87f0a66fe58c475f9
jayho-k <sox3579@gmail.com>
2023-02-16 20:53:16 +0900

    0216

--- PS/Back_jun/itertools_practice.py
+++ PS/Back_jun/itertools_practice.py
@@ -1,3 +1,14 @@
+'''
+3 3
+1 1 1
+2 2 2
+0 1 0
+3 3 3
+4 4 4
+5 5 100
+
+ 
+'''
 from itertools import permutations
 from itertools import combinations
 from itertools import product
@@ -5,10 +16,22 @@ from itertools import accumulate
 from itertools import combinations_with_replacement as cwr
 from collections import Counter
 from pprint import pprint
+def arrAdd(arr1,arr2):
+    print([list(map(sum,zip(ar1,ar2))) for ar1,ar2 in zip(arr1,arr2)])
 
 
-tu = (1,2,3,4,5,1,1,1,1,1,1,1,1)
-print(Counter(tu))
+n,m = map(int,input().split())
+arr1 = [list(map(int,input().split())) for _ in range(n)]
+arr2 = [list(map(int,input().split())) for _ in range(n)]
+arrAdd(arr1,arr2)
+
+
+
+
+
+
+# tu = (1,2,3,4,5,1,1,1,1,1,1,1,1)
+# print(Counter(tu))
 
 
 
