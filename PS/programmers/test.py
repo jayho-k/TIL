bac2b8d540ba143764036acafe1e2c30f1ad6118
jayho-k <sox3579@gmail.com>
2023-02-23 15:33:03 +0900

    0223

--- PS/programmers/test.py
+++ PS/programmers/test.py
@@ -1,13 +1,31 @@
+
+from pprint import pprint
+dic = {
+    "A" : set(),
+    "E" : set(),
+    "I" : set(),
+    "O" : set(),
+    "U" : set()
+}
+
 def dfs(d,com,word,full):
-    
     if d==5:
-        print(com)
+        if len(com)>0:
+            dic[com[0]].add(com)
         return
     
     for i in range(5):
-        # dfs(d+1,com,word,full)
+        dfs(d+1,com,word,full)
         dfs(d+1,com+full[i],word,full)
 
-full = ['A','E','I','O','U']
+
+full = [ 'A', 'E', 'I', 'O', 'U']
 word ='asdf'
 dfs(0,'',word,full)
+for f in full:
+    dic[f] = [sorted(list(dic[f])),len(dic[f])]
+
+print(dic["E"])
+
+
+
