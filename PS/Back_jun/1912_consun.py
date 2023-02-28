5bce2b11f261877c49ae4fa8c83181a4a4e0b87d
jayho-k <sox3579@gmail.com>
2023-02-28 17:31:15 +0900

    0228

--- PS/Back_jun/1912_consun.py
+++ PS/Back_jun/1912_consun.py
@@ -3,45 +3,13 @@
 10 -4 3 1 5 6 -35 12 21 -1
 
 '''
-
+from itertools import accumulate
 n = int(input())
-
-lst = list(map(int,input().split()))
-
+lst = list(accumulate(map(int,input().split())))
 mx = lst[0]
-sm = lst[0]
-
-
-
-
-
-    # if sm+lst[end] < mx:
-    #     mx = max(mx, sm+lst[end])
-    #     sm += lst[end]
-    #     end += 1
-
-    # else:
-    #     mx = max(mx, sm+lst[end])
-    #     sm -= lst[st]
-    #     st += 1
-
-
-
-
-
-
-# n = int(input())
-
-# lst = list(map(int,input().split()))
-
-# mx = lst[0]
-
-
-# for i in range(1,len(lst)):
-#     std = lst[i]
-#     for j in range(i-1,-1,-1):
-#         std += lst[j]
-#         mx = max(mx, std)
-
-
-# print(mx)
+j = 0
+for i in range(1,n):
+    mx = max(lst[i], lst[i]-lst[j],mx)
+    if lst[i]<lst[j]:
+        j=i
+print(mx)
