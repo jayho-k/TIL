d7873aa525a2a16fa09ca47a2a44d36d343d18e2
jayho-k <sox3579@gmail.com>
2023-03-01 20:42:16 +0900

    0301

---
+++ PS/swea/swea_1961.py
@@ -0,0 +1,40 @@
+'''
+90 180 270도 회전
+
+10
+3
+1 2 3
+4 5 6
+7 8 9
+6
+6 9 4 7 0 5
+8 9 9 2 6 5
+6 8 5 4 9 8
+2 2 7 7 8 4
+7 5 1 9 7 9
+8 9 3 9 7 6
+
+'''
+from pprint import pprint
+
+def rotate(n,board):
+    n_board = [[0]*n for _ in range(n)]
+    for y in range(n):
+        for x in range(n):
+            n_board[y][x] = board[n-1-x][y]
+    return n_board
+
+for tc  in range(1,int(input())+1):
+    n = int(input())
+    board = [list(map(int,input().split())) for _ in range(n)]
+    ans_baord = [[] for _ in range(3)]
+    for i in range(3):
+        board = rotate(n,board)
+        for y in range(n):
+            ans_baord[i].append(''.join(map(str,board[y])))
+
+    # ans
+    print(f'#{tc}')
+    pprint(ans_baord)
+    for a in zip(*ans_baord):
+        print(*a)
--- Spring/JPA/07_Association_Mapping1.md
+++ Spring/JPA/07_Association_Mapping1.md
@@ -299,8 +299,17 @@ public class Member{
     public void changeTeam(Team team){
         this.team = team;
         team.getMembers().add(this);
-    }    
+    }
 }
+
+
+
+
+// 반대쪽
+    public void addOrderItem(OrderItem orderItem){
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
 ```
 
 
