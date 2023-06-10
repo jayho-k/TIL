846512520065e74beff48278749a5051430e0f47
jayho-k <sox3579@gmail.com>
2023-06-10 19:42:56 +0900

    0610

--- PS/Back_jun/16236_baby_shark.py
+++ PS/Back_jun/16236_baby_shark.py
@@ -1,145 +1,232 @@
 '''
-1. 처음 상어의 크기: 2
-2. 물고기가 더 크면 먹을수 있음
-3. 물고기보다 작으면 지나갈 수 없음
-4. 크기 =2 ==> 2마리먹으면 3으로 진화
-
-이동방법
-1. 물고기 한마리 => 바로 먹으러 감
-2. 물고기 > 1   ==> 가까운거 부터 먹음
-3. 위 왼쪽
-
-lst 다 넣지 말고 bfs로 최소 값 가능! 한번 구현
+4
+4 3 2 1
+0 0 0 0
+0 0 9 0
+1 2 3 4
 
 '''
-# bfs
-from pprint import pprint
-from collections import deque
 
-def stp():
+from collections import deque
+from pprint import pprint
+
+def find_shark():
+    shy,shx = -1,-1
+    fish_grid = [[0]*n for _ in range(n)]
     for y in range(n):
         for x in range(n):
-            if grid[y][x] == 9:
-                grid[y][x] = 0
-                return y, x
+            if grid[y][x]==9:
+                shy,shx = y,x
+            elif grid[y][x]:
+                fish_grid[y][x]=grid[y][x]
 
-def bfs(y,x,shark):
-    q = deque([(y,x)])
+    return shy,shx,fish_grid
+
+def bfs(y,x):
+    
+    q = deque([(0,y,x)])
     visited = [[0]*n for _ in range(n)]
-    visited[y][x] = 1
-    d_lst = []
-
-    dy = [-1,1,0,0]
-    dx = [0,0,-1,1]
+    visited[y][x]=1
+    eat_lst = []
 
     while q:
-        y,x = q.popleft()
+        cnt,y,x = q.popleft()
+
         for d in range(4):
-            ny = y + dy[d]
-            nx = x + dx[d]
-            
-            # 이아기가 통로를 지나갈 수 있니? 물고기를 지나갈 수 있니??
-            if 0<=ny<n and 0<=nx<n and grid[ny][nx] <= shark and visited[ny][nx] == 0:
-                visited[ny][nx] = visited[y][x] + 1
-                q.append((ny,nx))
+            ny = y+dy[d]
+            nx = x+dx[d]
 
-            # 물고기를 먹을 수 있니?
-            if 0<=ny<n and 0<=nx<n and 0 < grid[ny][nx] < shark:
-                visited[ny][nx] = visited[y][x] + 1
-                d =  visited[ny][nx] - 1
-                d_lst.append((d,ny,nx))
-                # grid[ny][nx] = 0
+            if 0<=ny<n and 0<=nx<n and visited[ny][nx]==0:
+                if shark==fish_grid[ny][nx] or fish_grid[ny][nx]==0:
+                    visited[ny][nx]=1
+                    q.append((cnt+1,ny,nx))
 
-    if d_lst:
-        d_lst.sort()
-        d,y,x = d_lst[0]
-        grid[y][x] = 0
-        return d,y,x
+                elif shark>fish_grid[ny][nx]:
+                    visited[ny][nx]=1
+                    q.append((cnt+1,ny,nx))
+                    eat_lst.append((cnt+1,ny,nx))
+    
+    if eat_lst:
+        eat_lst.sort(key=lambda x : (x[0],x[1],x[2]))
+        cnt,ey,ex = eat_lst[0]
+        fish_grid[ey][ex] = 0
+        return ey,ex,cnt,True
 
     else:
-        return 0,ny,nx    
+        return -1,-1,0,False
+
 
 
 n = int(input())
-grid = [list(map(int, input().split())) for _ in range(n)]
-sty, stx = stp()
-
-# stp를 초기화시켜주기
-# while문을 사용한다면 break를 언제 걸어줘야 하나?
-
+grid = [list(map(int,input().split())) for _ in range(n)]
 shark = 2
-cnt = 0
-total_d = 0
-while 1:
-
-    d, y, x = bfs(sty,stx, shark)
-    if d == 0:
-        break
-    sty = y
-    stx = x
-    cnt += 1
-    total_d += d
-    if shark == cnt:
-        shark += 1
-        cnt = 0
-
-print(total_d)
+eatten = 0
+total = 0
+dy = [-1,0,1,0]
+dx = [0,1,0,-1]
+shy,shx,fish_grid = find_shark()
 
 
-
-'''
-거리만으로 구하기
-이렇게 풀면 상어가 본인보다 큰 물고기도 그냥 지나치게 된다.
-따라서 이 방법은 막힌것이 없을때 사용하는 것이 좋아 보인다.
-'''
-
-from sys import stdin
-from collections import deque
-from pprint import pprint
-
-def stp():
-    for y in range(n):
-        for x in range(n):
-            if grid[y][x] == 9:
-                return y,x
-
-
-n = int(stdin.readline())
-grid = [list(map(int,stdin.readline().split())) for _ in range(n)]
-sty,stx = stp()
-
-mxd = 0
-shark = 2
-cnt = 0
-
-while 1:
-    d_lst = deque([])
-    mnd = mny = mnx = 1e9
-
-    for y in range(n):
-        for x in range(n):
-            if 0 < grid[y][x] < shark:
-                d = abs(sty-y) +abs(stx-x)
-                d_lst.append((d,y,x))
+while True:
+    shy,shx,cnt,flag = bfs(shy,shx)
+    total+=cnt
+    if flag:
+        eatten+=1
         
-    if not d_lst:
+        if shark==eatten:
+            shark+=1
+            eatten=0
+    else:
         break
 
-    print(d_lst)
-    for d,y,x in d_lst:
-        if mnd > d:
-            mnd = d
-            mny = y
-            mnx = x
+print(total)
 
-    mxd += mnd
-    sty, stx = mny, mnx
-    pprint(grid)
-    print('sh',shark)
-    grid[sty][stx] = 0
-    cnt += 1
-    if shark == cnt:
-        shark += 1
-        cnt = 0
 
-print(mxd)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # bfs
+# from pprint import pprint
+# from collections import deque
+
+# def stp():
+#     for y in range(n):
+#         for x in range(n):
+#             if grid[y][x] == 9:
+#                 grid[y][x] = 0
+#                 return y, x
+
+# def bfs(y,x,shark):
+#     q = deque([(y,x)])
+#     visited = [[0]*n for _ in range(n)]
+#     visited[y][x] = 1
+#     d_lst = []
+
+#     dy = [-1,1,0,0]
+#     dx = [0,0,-1,1]
+
+#     while q:
+#         y,x = q.popleft()
+#         for d in range(4):
+#             ny = y + dy[d]
+#             nx = x + dx[d]
+            
+#             # 이아기가 통로를 지나갈 수 있니? 물고기를 지나갈 수 있니??
+#             if 0<=ny<n and 0<=nx<n and grid[ny][nx] <= shark and visited[ny][nx] == 0:
+#                 visited[ny][nx] = visited[y][x] + 1
+#                 q.append((ny,nx))
+
+#             # 물고기를 먹을 수 있니?
+#             if 0<=ny<n and 0<=nx<n and 0 < grid[ny][nx] < shark:
+#                 visited[ny][nx] = visited[y][x] + 1
+#                 d =  visited[ny][nx] - 1
+#                 d_lst.append((d,ny,nx))
+#                 # grid[ny][nx] = 0
+
+#     if d_lst:
+#         d_lst.sort()
+#         d,y,x = d_lst[0]
+#         grid[y][x] = 0
+#         return d,y,x
+
+#     else:
+#         return 0,ny,nx    
+
+
+# n = int(input())
+# grid = [list(map(int, input().split())) for _ in range(n)]
+# sty, stx = stp()
+
+# # stp를 초기화시켜주기
+# # while문을 사용한다면 break를 언제 걸어줘야 하나?
+
+# shark = 2
+# cnt = 0
+# total_d = 0
+# while 1:
+
+#     d, y, x = bfs(sty,stx, shark)
+#     if d == 0:
+#         break
+#     sty = y
+#     stx = x
+#     cnt += 1
+#     total_d += d
+#     if shark == cnt:
+#         shark += 1
+#         cnt = 0
+
+# print(total_d)
+
+
+
+# '''
+# 거리만으로 구하기
+# 이렇게 풀면 상어가 본인보다 큰 물고기도 그냥 지나치게 된다.
+# 따라서 이 방법은 막힌것이 없을때 사용하는 것이 좋아 보인다.
+# '''
+
+# from sys import stdin
+# from collections import deque
+# from pprint import pprint
+
+# def stp():
+#     for y in range(n):
+#         for x in range(n):
+#             if grid[y][x] == 9:
+#                 return y,x
+
+
+# n = int(stdin.readline())
+# grid = [list(map(int,stdin.readline().split())) for _ in range(n)]
+# sty,stx = stp()
+
+# mxd = 0
+# shark = 2
+# cnt = 0
+
+# while 1:
+#     d_lst = deque([])
+#     mnd = mny = mnx = 1e9
+
+#     for y in range(n):
+#         for x in range(n):
+#             if 0 < grid[y][x] < shark:
+#                 d = abs(sty-y) +abs(stx-x)
+#                 d_lst.append((d,y,x))
+        
+#     if not d_lst:
+#         break
+
+#     print(d_lst)
+#     for d,y,x in d_lst:
+#         if mnd > d:
+#             mnd = d
+#             mny = y
+#             mnx = x
+
+#     mxd += mnd
+#     sty, stx = mny, mnx
+#     pprint(grid)
+#     print('sh',shark)
+#     grid[sty][stx] = 0
+#     cnt += 1
+#     if shark == cnt:
+#         shark += 1
+#         cnt = 0
+
+# print(mxd)
--- PS/swea/swea_2477.py
+++ PS/swea/swea_2477.py
@@ -1,93 +1,112 @@
 '''
+1
+2 2 6 1 2
+3 2
+4 2
+0 0 1 2 3 4
 
-
-
-
+1
+4 2 30 4 2
+3 2 2 10
+2 6
+0 2 2 4 5 6 7 9 9 15 15 16 17 18 18 19 19 22 23 24 24 24 25 25 25 26 27 27 28 29
 '''
+
 from collections import deque
-
-def go_rep(wait):
-    repair_empty = [0]*len(repair)
-    waitq = deque(wait)
-    timer = 0
-
-    res = []
-    
-    while waitq:
-
-        timer += 1
-        rp_empty = 0
-        for rj in range(len(repair_empty)):
-            if repair_empty[rj]:
-                repair_empty[rj]-=1
-
-            if repair_empty[rj]==0:
-                rp_empty+=1
-
-        while rp_empty and waitq and waitq[0][0]<timer:
-            wt,wcust,rg_num = waitq.popleft()
-            for rpi in range(len(repair_empty)):
-                if repair_empty[rpi]==0:
-                    repair_empty[rpi]=repair[rpi]
-                    res.append((rg_num,rpi,wcust))
-                    rp_empty-=1
-                    break
-    return res
 
 def go_regi():
     
-    timer = 0
-    cusq = deque()
-    for i in range(len(custom)):
-        cusq.append((custom[i],i))
+    time=0
+    wait_lst = deque()
+    complete_lst = [] # time,e,p_num
+    empty = [0]*len(regi) #[[time,p_num]*len(regi)]
 
-    regi_empty = [0]*len(regi)
-    wait = []
-    while cusq:
+    for p in range(len(person)):
+        wait_lst.append((person[p],p))
 
-        timer+=1
-        
-        # 창구에 존재 + 비어있는지
-        rg_empty = 0
-        for ri in range(len(regi_empty)):
-            if regi_empty[ri]:
-                regi_empty[ri]-=1
 
-            if regi_empty[ri]==0:
-                rg_empty+=1
-        
-        # 창구에 들어가기
-        # 창구가 비어있고, 고객이 존재하고, 고객의 시간이 맞을 경우
-        while rg_empty and cusq and cusq[0][0]<timer:
-            t,cnum = cusq.popleft()
-            for rei in range(len(regi_empty)):
-                if regi_empty[rei]==0:
-                    regi_empty[rei]=regi[rei] #걸리는 시간체크
+    while wait_lst:
+        time+=1
 
-                    # 총걸린 시간 = 창구에서 일한 시간 + 현재시간
-                    # t는 고객이 도착한 시간이잖아 => 그럼 기다린 시간이 포함되지 않음
-                    wait.append((regi[rei]+timer,cnum,rei)) #총걸린 시간, 고객번호, 창구번호
-                    rg_empty-=1
+        # 시간 줄이기
+        empty_cnt = 0
+        for e1 in range(len(empty)):
+            if empty[e1]:
+                empty[e1]-=1
+
+            if empty[e1]==0:
+                empty_cnt+=1
+        # 창구가 비었을 경우 추가해줘야함
+        while empty_cnt and wait_lst and wait_lst[0][0]<time:
+            
+            # 들어온 시간
+            _,p_num = wait_lst.popleft()
+            
+            # 창구 들어가기
+            for e2 in range(len(empty)):
+                if empty[e2]==0:
+                    empty[e2]=regi[e2]
+                    complete_lst.append((time+regi[e2],e2,p_num))
+                    empty_cnt-=1
                     break
+    
+    complete_lst.sort(key=lambda x : (x[0],x[1]))
+    # print(complete_lst)
+    return complete_lst
 
-    wait.sort(key=lambda x : (x[0],x[2]))
-    # print(wait)
-    return wait
+def go_fix(comlete_lst):
+    
+    time=0
+    wait_lst = deque(comlete_lst)  # time,regi_num,p_num
+    print(comlete_lst)
+    complete = [] 
+    empty = [0]*len(fix)
+    while wait_lst:
+
+        time+=1
+        empty_cnt = 0
+
+        for e1 in range(len(empty)):
+            if empty[e1]!=0:
+                empty[e1]-=1
+
+            if empty[e1]==0:
+                empty_cnt+=1
+
+        # print(empty)
+        # print(empty_cnt)
+        while empty_cnt and wait_lst and wait_lst[0][0] < time:
+            _,regi_num,p_num = wait_lst.popleft()
+
+            # fix들어가기
+            for e2 in range(len(empty)):
+                if empty[e2]==0:
+                    empty[e2]=fix[e2]
+                    empty_cnt-=1
+                    complete.append((regi_num,e2,p_num))
+                    break
+    # print(wait_lst)
+    return complete
+
 
 
 for tc in range(1,int(input())+1):
-
-    # 접수, 정비, 고객, 지갑접수, 지갑정비
     n,m,k,a,b = map(int,input().split())
     regi = list(map(int,input().split()))
-    repair = list(map(int,input().split()))
-    custom = list(map(int,input().split()))
-    wait = go_regi()
-    res = go_rep(wait)
-    total = 0
-    for rg_num,rp_num,wcust in res:
-        if a == rg_num+1 and b == rp_num+1:
-            total += wcust+1
+    fix = list(map(int,input().split()))
+    person = list(map(int,input().split()))
+    total = 0 
+
+    complete_lst = go_regi()
+    res = go_fix(complete_lst)
+    
+    # print(a,b)
+    # print(res)
+    for rn,fn,pn in res:
+        if a==rn+1 and b==fn+1:
+            print(rn+1,fn+1)
+            total+=pn+1
+
     if total:
         print(f"#{tc} {total}")
     else:
@@ -96,7 +115,41 @@ for tc in range(1,int(input())+1):
 
 
 
+# from collections import deque
 
+# def go_rep(wait):
+#     repair_empty = [0]*len(repair)
+#     waitq = deque(wait)
+#     timer = 0
+
+#     res = []
+    
+#     while waitq:
+
+#         timer += 1
+#         rp_empty = 0
+#         for rj in range(len(repair_empty)):
+#             if repair_empty[rj]:
+#                 repair_empty[rj]-=1
+
+#             if repair_empty[rj]==0:
+#                 rp_empty+=1
+#         # print("timer : ",timer)
+#         # print("check전",repair_empty)
+#         # print("rp_empty : ",rp_empty)
+#         while rp_empty and waitq and waitq[0][0]<timer:
+#             wt,wcust,rg_num = waitq.popleft()
+#             for rpi in range(len(repair_empty)):
+#                 if repair_empty[rpi]==0:
+#                     repair_empty[rpi]=repair[rpi]
+#                     res.append((rg_num,rpi,wcust))
+#                     rp_empty-=1
+#                     break
+#         # print("check후",repair_empty)
+#         # print(wait)
+#         # print(res)
+#         # print()
+#     return res
 
 # def go_regi():
     
@@ -106,31 +159,12 @@ for tc in range(1,int(input())+1):
 #         cusq.append((custom[i],i))
 
 #     regi_empty = [0]*len(regi)
-#     repair_empty = [0]*len(repair)
-#     waitq = deque()
-
-#     res = []
-#     while len(res)<len(custom):
-
-#         timer += 1
-#         rp_empty = 0
-#         # print(timer)
-#         for rj in range(len(repair_empty)):
-#             if repair_empty[rj]:
-#                 repair_empty[rj]-=1
-
-#             if repair_empty[rj]==0:
-#                 rp_empty+=1
-
-#         while rp_empty and waitq and waitq[0][0]<timer:
-#             wt,wcust,rg_num = waitq.popleft()
-#             for rpi in range(len(repair_empty)):
-#                 if repair_empty[rpi]==0:
-#                     repair_empty[rpi]=repair[rpi]-1
-#                     res.append((rg_num,rpi,wcust))
-#                     rp_empty-=1
-#                     break
+#     wait = []
+#     while cusq:
+#         # 어떤 것으로 break걸지 생각하기
+#         timer+=1
         
+#         # 창구에 존재 + 비어있는지
 #         rg_empty = 0
 #         for ri in range(len(regi_empty)):
 #             if regi_empty[ri]:
@@ -138,16 +172,43 @@ for tc in range(1,int(input())+1):
 
 #             if regi_empty[ri]==0:
 #                 rg_empty+=1
-
+#         # 창구에 들어가기
+#         # 창구가 비어있고, 고객이 존재하고, 고객의 시간이 맞을 경우
+#         # print("check전",regi_empty)
+#         # print("empty : ",empty)
 #         while rg_empty and cusq and cusq[0][0]<timer:
 #             t,cnum = cusq.popleft()
 #             for rei in range(len(regi_empty)):
 #                 if regi_empty[rei]==0:
 #                     regi_empty[rei]=regi[rei] #걸리는 시간체크
-#                     waitq.append((regi[rei]+t,cnum,rei)) #총걸린 시간, 고객번호, 창구번호
+#                     wait.append((regi[rei]+timer,cnum,rei)) #총걸린 시간, 고객번호, 창구번호
 #                     rg_empty-=1
 #                     break
-#         waitq = deque(sorted(list(waitq)))
+#         # print("check후",regi_empty)
+#         # print()
 
-#         # print(res)
-#     return res
+#     wait.sort(key=lambda x : (x[0],x[2]))
+#     # print(wait)
+#     return wait
+
+
+# for tc in range(1,int(input())+1):
+#     # 접수, 정비, 고객, 지갑접수, 지갑정비
+#     n,m,k,a,b = map(int,input().split())
+#     regi = list(map(int,input().split()))
+#     repair = list(map(int,input().split()))
+#     custom = list(map(int,input().split()))
+#     wait = go_regi()
+#     res = go_rep(wait)
+#     total = 0
+#     # print("wait",wait)
+#     # print(a,b)
+#     # print("res", res)
+#     for rg_num,rp_num,wcust in res:
+#         if a == rg_num+1 and b == rp_num+1:
+#             # print('rg_num , rp_num : ',rg_num,rp_num, wcust)
+#             total += wcust+1
+#     if total:
+#         print(f"#{tc} {total}")
+#     else:
+#         print(f"#{tc} {-1}")
