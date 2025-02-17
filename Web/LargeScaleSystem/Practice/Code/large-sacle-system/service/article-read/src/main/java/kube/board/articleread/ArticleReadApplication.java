16a888e725e137f52f57cf28a871286b7930f31a
jayho-k <sox3579@gmail.com>
2025-02-17 23:15:24 +0900

    20250217

---
+++ Web/LargeScaleSystem/Practice/Code/large-sacle-system/service/article-read/src/main/java/kube/board/articleread/ArticleReadApplication.java
@@ -0,0 +1,13 @@
+package kube.board.articleread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticleReadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArticleReadApplication.class, args);
+    }
+
+}
---
+++ Web/LargeScaleSystem/Practice/Code/large-sacle-system/service/hot-article/src/main/java/kube/board/hotarticle/HotArticleApplication.java
@@ -0,0 +1,13 @@
+package kube.board.hotarticle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotArticleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotArticleApplication.class, args);
+    }
+
+}
